f78a0a38e32814ed25aebd6b1a9d98ea47451827
anamary <1225733380@qq.com>
2014-12-21 20:13:57 +0800

    ceshi

--- resource/javascripts/business/index/index.js
+++ resource/javascripts/business/index/index.js
@@ -1,4 +1,102 @@
 $(document).ready(function() {
+    var login = function() {
+        _gthis = this;
+        _gthis.username = "";
+        _gthis.password = "";
+    };
+    login.prototype = {
+        _init: function() {
+            _gthis._checklogin();
+        },
+        _checklogin: function() {
+            var sa = $.cookie('sa');
+            if (sa == '' || sa == null) {
+                _gthis._glogin();
+            } else {
+                $.ajax({
+                    type: 'GET',
+                    url: 'http://sa.kascend.com/auth/info',
+                    success: function(data) {
+                        var profile = data.profile;
+                        var show = [];
+                        profile.forEach(function(one, index) {
+                            var b = {
+                                url: one.url,
+                                title: one.title
+                            };
+                            show.push(b);
+                        });
+                        var template = $("#firstdata").html();
+                        Mustache.parse(template);
+                        var rendered = Mustache.render(template, show);
+                        $(".con_2 .one_mid ul").html(rendered);
+                        $(".logincontainer").hide();
+                        var newpanel = new panel();
+                        newpanel._init();
+                    },
+                    error: function(error) {
+                        _gthis._glogin();
+                    }
+                });
+            }
+        },
+        _glogin: function() {
+            _gthis._op();
+        },
+        _op: function() {
+            $(".loginbtn").click(function() {
+                _gthis._getValue();
+                if (_gthis._checkValue()) {
+                    _gthis._beginLogin();
+                } else {
+                    alert("不能为空");
+                }
+            });
+        },
+        _getValue: function() {
+            _gthis.username = $(".username").val();
+            _gthis.password = $(".password").val();
+        },
+        _checkValue: function() {
+            if (_gthis.username == "" || _gthis.password == "") {
+                return false;
+            } else {
+                return true;
+            }
+        },
+        _beginLogin: function() {
+            var data = {
+                username: _gthis.username,
+                password: _gthis.password
+            };
+            $.ajax({
+                type: 'POST',
+                url: 'http://sa.kascend.com/auth/login',
+                data: data,
+                dataType: 'json',
+                success: function(data) {
+                    var profile = data.profile;
+                    var show = [];
+                    profile.forEach(function(one, index) {
+                        var b = {
+                            url: one.url,
+                            title: one.title
+                        };
+                        show.push(b);
+                    });
+                    var template = $("#firstdata").html();
+                    Mustache.parse(template);
+                    var rendered = Mustache.render(template, show);
+                    $(".con_2 .one_mid ul").html(rendered);
+                    $(".logincontainer").hide();
+                    var newpanel = new panel();
+                    newpanel._init();
+                }
+            });
+        }
+    };
+
+
     var panel = function() {
         _pthis = this;
     };
@@ -31,8 +129,9 @@ $(document).ready(function() {
                     dataType: 'json',
                     success: function(callback) {
                         var back = callback;
+                        var profile = back.profile;
                         var show = [];
-                        back.profile.forEach(function(one, index) {
+                        profile.forEach(function(one, index) {
                             var b = {
                                 url: one.url,
                                 title: one.title
@@ -91,4 +190,6 @@ $(document).ready(function() {
             }
         }
     };
+    var newlogin = new login();
+    newlogin._init();
 });
